556ca157ec37a5ed63b2bcb97f5573ddca0bfac4
Kemo Jallow <kemojal@users.noreply.github.com>
2019-01-30 21:16:43 +0800

    commit

---
+++ apple-red/html/red/assets/js/scripts.js
@@ -0,0 +1,46 @@
+/*!
+
+ *
+ * script.js is a script that controls the scrolling animations and background 
+ * slider
+ */
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+	
+    /*
+        Background slideshow
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    $('.section-buy-container').backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow js library for scrolling animation
+    */
+    new WOW().init();
+	
+});
